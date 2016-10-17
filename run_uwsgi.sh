560ebf8346284f7bb2e5f371ba1dbbfef19495b7
Serafeim Papastefanos <spapas@gmail.com>
2016-10-17 14:22:17 +0300

    Fix props

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,1 +1,1 @@
-uwsgi --http-socket=0.0.0.0:8001 --module=mailer_server.wsgi:application --processes=2 --master=true
+uwsgi --http-socket=0.0.0.0:8001 --module=mailer_server.wsgi:application --processes=2 --master

253d9a45f2e8eb3b36e353d4e51621fc1b2f90e3
Serafeim Papastefanos <spapas@gmail.com>
2016-11-03 13:47:14 +0200

    Fix uat settings more

--- mailer_server/settings/uat.py
+++ mailer_server/settings/uat.py
@@ -3,9 +3,20 @@ from .base import *
 DEBUG = False
 SITE_ID = 2
 
+COMPRESS_OFFLINE = True
+
 INSTALLED_APPS += (
     'raven.contrib.django.raven_compat',
 )
+
+INSTALLED_APPS.insert(0, 'whitenoise.runserver_nostatic', )
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
 
 CSRF_COOKIE_SECURE = False # Override CSRF to work also with http
 SESSION_COOKIE_SECURE = False # Override session to work also with http

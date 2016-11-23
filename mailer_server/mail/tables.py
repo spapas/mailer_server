6c8734150acfe8eb4bbf1b62da4e930ab489f4d9
Serafeim Papastefanos <spapas@gmail.com>
2016-11-23 14:16:51 +0200

    Some small things

--- mailer_server/mail/tables.py
+++ mailer_server/mail/tables.py
@@ -19,13 +19,19 @@ class MailTemplateTable(tables.Table):
 
 
 class MailTable(tables.Table):
+    mail_template = LinkColumn('mt_mail_mailtemplate_detail', args=[A('mail_template_id')])
+    
     class Meta:
         model = Mail
         attrs = {'class': 'table'}
-        order_by = ('-created_on', )
+        order_by = ('-id', )
 
 
 class MassMailTable(tables.Table):
+    mail_template = LinkColumn('mt_mail_mailtemplate_detail', args=[A('mail_template_id')])
+    distribution_list_to = LinkColumn('dl_mail_distributionlist_detail', args=[A('distribution_list_to_id')])
+
     class Meta:
         model = MassMail
         attrs = {'class': 'table'}
+        order_by = ('-id', )

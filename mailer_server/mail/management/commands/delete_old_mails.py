e7a968a7fda0636e5a7dc00cb8a1cc55be3684c8
Serafeim Papastefanos <spapas@gmail.com>
2017-01-04 10:55:53 +0200

    Add management command to remove old mails

---
+++ mailer_server/mail/management/commands/delete_old_mails.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from mailer_server.mail.models import Mail
+import datetime
+
+class Command(BaseCommand):
+    help = 'Removes old mails from database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('days', type=int, help='Remove mails older than N days')
+
+    def handle(self, *args, **options):
+        
+        date = datetime.date.today() - datetime.timedelta(days=options['days'])
+        mails = Mail.objects.filter(created_on__lte=date)
+        
+        print "Will delete mails older than", date, "i.e", len(mails), "mails."
+        mails.delete()

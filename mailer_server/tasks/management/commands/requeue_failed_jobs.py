464a5a2fa61d00c5fc286d139bba6f228e684529
Serafeim Papastefanos <spapas@gmail.com>
2017-10-02 08:41:25 +0300

    Add a requeue failed jobs management cmd

--- mailer_server/tasks/management/commands/requeue_failed_jobs.py
+++ mailer_server/tasks/management/commands/requeue_failed_jobs.py
@@ -6,9 +6,26 @@ class Command(BaseCommand):
     help = 'Requeue all failed jobs'
 
     def add_arguments(self, parser):
-        parser.add_argument('dry', help='Dry run')
+        parser.add_argument('dry', nargs='?', help='Dry run')
 
     def handle(self, *args, **options):
-        print options['dry']
-        
+        dry = options['dry']
+
+        print "Starting requeue failed jobs at {0} with {1}".format(
+            datetime.datetime.now(),
+            "dry" if dry else "no dry"
+        )
+
         fq = get_failed_queue()
+
+        tot = 0
+        for ji in fq.job_ids:
+            tot += 1
+            if not dry:
+                print "Requeing {0}".format(ji)
+                fq.requeue(ji)
+
+        print "Finished requeue failed jobs at {0} - requeued {1} jobs (if not dry)".format(
+            datetime.datetime.now(),
+            tot
+        )

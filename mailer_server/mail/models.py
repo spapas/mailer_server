921d6bb02f4c28a7a063696628c6ac68aff2a80c
Serafeim Papastefanos <spapas@gmail.com>
2019-06-26 12:53:33 +0300

    Fix missing attr

--- mailer_server/mail/models.py
+++ mailer_server/mail/models.py
@@ -54,7 +54,7 @@ class Mail(models.Model):
             subject=self.subject,
             body=self.body,
             from_email=self.mail_from,
-            to=self.mail_to.split(',')  if self.to else None,
+            to=self.mail_to.split(',')  if self.mail_to else None,
             bcc=self.bcc.split(',') if self.bcc else None,
             connection=connection,
             attachments=attachments,

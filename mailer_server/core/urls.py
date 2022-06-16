9def893a2b964c92101def6465023c3fa84907db
Serafeim Papastefanos <spapas@gmail.com>
2022-06-16 11:15:46 +0300

    add error url for test

--- mailer_server/core/urls.py
+++ mailer_server/core/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth import login, logout
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-
+from django.contrib.auth.decorators import permission_required
 from mailer_server.core.views import CreateTokenFormView
 
 urlpatterns = [
@@ -17,4 +17,5 @@ urlpatterns = [
     url(r'login/$', auth_views.LoginView.as_view(), name='auth_login'),
     url(r'logout/$', auth_views.LogoutView.as_view(template_name='logout.html'), name='auth_logout'),
 
+    url(r'error/$', permission_required('core.admin')(lambda r: a+1), name='error')
 ]

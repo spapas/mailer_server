f686a6b08da5e1086e51aa8a12962d98a93a09aa
Serafeim Papastefanos <spapas@gmail.com>
2023-02-17 12:43:50 +0200

    improve email validation

--- mailer_server/mail/serializers.py
+++ mailer_server/mail/serializers.py
@@ -5,41 +5,66 @@ from mailer_server.mail.models import Mail, MassMail
 
 def email_valid(s):
     _, email = parseaddr(s)
-    if email == '' or not '@' in email:
+    if (
+        email == ""
+        or "@" not in email
+        or "." not in email
+        or email.count("@") > 1
+        or "\n" in email
+    ):
         return False
     return True
 
 
 class MailSerializer(serializers.ModelSerializer):
-    attachment = serializers.FileField(write_only=True, required=False, )
+    attachment = serializers.FileField(
+        write_only=True,
+        required=False,
+    )
 
     class Meta:
         model = Mail
         fields = (
-            'id', 'subject', 'body', 'mail_from',
-            'mail_to', 'cc', 'bcc', 'body_type',
-            'created_on', 'created_by', 'reply_to', 'attachment',
-
+            "id",
+            "subject",
+            "body",
+            "mail_from",
+            "mail_to",
+            "cc",
+            "bcc",
+            "body_type",
+            "created_on",
+            "created_by",
+            "reply_to",
+            "attachment",
         )
-        read_only_fields = ('id', 'created_on', 'created_by',)
+        read_only_fields = (
+            "id",
+            "created_on",
+            "created_by",
+        )
 
     def validate(self, data):
-        for f in ['mail_from', 'reply_to']:
+        for f in ["mail_from", "reply_to"]:
             fval = data.get(f)
             if fval:
                 if not email_valid(fval):
-                    raise serializers.ValidationError("Cannot parse address {0}".format(fval))
+                    raise serializers.ValidationError(
+                        "Cannot parse address {0}".format(fval)
+                    )
 
-        for f in ['mail_to', 'cc', 'bcc']:
-            
+        for f in ["mail_to", "cc", "bcc"]:
+
             fval = data.get(f)
             print(f, fval)
             if fval:
-                fval_parts = fval.split(',')
+                fval_parts = fval.split(",")
                 for fvp in fval_parts:
                     print("Field original val and parsed ", fvp, parseaddr(fvp))
                     if not email_valid(fvp):
-                        raise serializers.ValidationError("Cannot parse address {0}".format(fvp))
+                        raise serializers.ValidationError(
+                            "Cannot parse address {0}".format(fvp)
+                        )
         return data
 
 
@@ -47,9 +72,14 @@ class MassMailSerializer(serializers.ModelSerializer):
     class Meta:
         model = MassMail
         fields = (
-            'id', 'mail_template', 
-            'distribution_list_to', 'created_on', 'created_by',
-
+            "id",
+            "mail_template",
+            "distribution_list_to",
+            "created_on",
+            "created_by",
         )
-        read_only_fields = ('id', 'created_on', 'created_by',)
-
+        read_only_fields = (
+            "id",
+            "created_on",
+            "created_by",
+        )

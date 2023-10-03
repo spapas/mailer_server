dc148f9f11416c5ac166cb32ae8c40eabbb8c69b
Serafeim Papastefanos <spapas@gmail.com>
2023-10-03 14:03:54 +0300

    fok

--- requeue_failed_jobs.sh
+++ requeue_failed_jobs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-source /home/serafeim/mailer_server/venv/bin/activate && python /home/serafeim/mailer_server/mailer_server/manage.py requeue_failed_jobs > /tmp/"requeue_failed_jobs`date +"_%Y-%m-%d"`.log"
+source /home/serafeim/mailer_server/venv/bin/activate && python /home/serafeim/mailer_server/mailer_server/manage.py requeue_failed_jobs >> /tmp/"requeue_failed_jobs`date +"_%Y-%m-%d"`.log"
+source /home/serafeim/mailer_server/venv/bin/activate && python /home/serafeim/mailer_server/mailer_server/manage.py delete_old_mails 100 >> /tmp/"requeue_failed_jobs`date +"_%Y-%m-%d"`.log"
 

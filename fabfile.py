91116658a3c3a43778c294fd8e1032a74f1a9f0b
Serafeim Papastefanos <spapas@gmail.com>
2024-10-10 10:33:15 +0300

    ok

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,8 @@ from mailer_server.settings.local import SUPERVISORD_PASSWORD
 def check_migrations():
     "Do migrations check"
     print("Checking migrations")
-    #local('python manage.py makemigrations --dry-run --check')
+    # local('python manage.py makemigrations --dry-run --check')
+
 
 def pep8():
     "Do pep8 styule checks"
@@ -15,37 +16,48 @@ def pep8():
     # local('pep8 --max-line-length=160 --filename=*.py mailer_server --exclude migrations')
     print("pep8 ok!")
 
+
 def commit():
-    local('git add .')
+    local("git add .")
     with settings(warn_only=True):
-        local('git commit')
+        local("git commit")
     with settings(warn_only=True):
-        local('git push origin master')
+        local("git push origin master")
     print("Commit ok")
+
 
 def pull():
     with cd(env.directory):
-        run('https_proxy=http://proxy.hcg.gr:8080 git fetch origin')
-        run('git merge origin/master')
+        run("https_proxy=http://proxy.hcg.gr:8080 git fetch origin")
+        run("git merge origin/master")
     print("fetch / merge ok")
+
 
 def work():
     "Do work on server (copy settings, migrate and run collect static)"
     with cd(env.directory):
-        requirements_txt = 'requirements/'+env.env+".txt"
+        requirements_txt = "requirements/" + env.env + ".txt"
         if os.stat(requirements_txt).st_size > 0:
-            virtualenv('https_proxy=http://proxy.hcg.gr:8080 pip install -r {0}'.format(requirements_txt) )
-        virtualenv('python manage.py migrate')
-        virtualenv('python manage.py update_permissions')
-        virtualenv('python manage.py compress --force')
-        virtualenv('python manage.py collectstatic --noinput')
+            virtualenv(
+                "https_proxy=http://proxy.hcg.gr:8080 pip install -r {0}".format(
+                    requirements_txt
+                )
+            )
+        virtualenv("python manage.py migrate")
+        virtualenv("python manage.py update_permissions")
+        virtualenv("python manage.py compress --force")
+        virtualenv("python manage.py collectstatic --noinput")
+
 
 def touch_gunicorn():
-    print("Restarting gunicorn");
-    
-    run('supervisorctl -s http://127.0.0.1:9001 -u serafeim -p '+SUPERVISORD_PASSWORD+'  status mlrsrvgunicorn ' + r'| sed "s/.*[pid ]\([0-9]\+\)\,.*/\1/" | xargs kill -HUP')
-    print("Restarting rqworker");
-    run('supervisorctl -s http://127.0.0.1:9001 -u serafeim -p '+SUPERVISORD_PASSWORD +' status mlrsrvrqworker ' + r'| sed "s/.*[pid ]\([0-9]\+\)\,.*/\1/" | xargs kill -HUP')
+    print("Restarting gunicorn")
+
+    # run('supervisorctl -s http://127.0.0.1:9001 -u serafeim -p '+SUPERVISORD_PASSWORD+'  status mlrsrvgunicorn ' + r'| sed "s/.*[pid ]\([0-9]\+\)\,.*/\1/" | xargs kill -HUP')
+    sudo("systemctl restart mlrsrv")
+    print("Restarting rqworker")
+    sudo("systemctl restart mlrsrv-rqworker")
+    # run('supervisorctl -s http://127.0.0.1:9001 -u serafeim -p '+SUPERVISORD_PASSWORD +' status mlrsrvrqworker ' + r'| sed "s/.*[pid ]\([0-9]\+\)\,.*/\1/" | xargs kill -HUP')
+
 
 def full_deploy():
     "Commit - pull - do work - and restart uwsgi"
@@ -56,23 +68,24 @@ def full_deploy():
     work()
     touch_gunicorn()
 
+
 def virtualenv(command):
-    run(env.activate + '&&' + command)
+    run(env.activate + "&&" + command)
 
 
 def uat():
     "UAT settings"
     env.env = "uat"
-    env.user = 'serafeim'
-    env.hosts = ['uat1.hcg.gr']
-    env.directory = '/home/serafeim/mailer_server/mailer_server'
-    env.activate = 'source /home/serafeim/mailer_server/venv/bin/activate'
+    env.user = "serafeim"
+    env.hosts = ["uat1.hcg.gr"]
+    env.directory = "/home/serafeim/mailer_server/mailer_server"
+    env.activate = "source /home/serafeim/mailer_server/venv/bin/activate"
+
 
 def prod():
     "PROD settings"
     env.env = "prod"
-    env.user = 'serafeim'
-    env.hosts = ['172.19.130.142']
-    env.directory = '/home/serafeim/mailer_server/mailer_server'
-    env.activate = 'source /home/serafeim/mailer_server/venv/bin/activate'
-
+    env.user = "serafeim"
+    env.hosts = ["172.19.130.142"]
+    env.directory = "/home/serafeim/mailer_server/mailer_server"
+    env.activate = "source /home/serafeim/mailer_server/venv/bin/activate"
